0743165a152f8881ef5aa32212fa82d51600e946
hactazoff <hactazia@gmail.com>
2023-06-23 03:59:17 +0200

    Autoredirect HTTPS

--- public/js/main.js
+++ public/js/main.js
@@ -1,2 +1,6 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+if (location.protocol !== 'https:' && !/(localhost|\b(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)(?::\d{0,4})?\b)/.test(location.hostname)) {
+    location.replace(`https:${location.href.substring(location.protocol.length)}`);
+}
